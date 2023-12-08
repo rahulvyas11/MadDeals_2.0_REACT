cfc0fed5868fa6c3df5f2bb87b1d7f6a919cff96
Rohit Singhal <rohitpsinghal@gmail.com>
2023-12-08 14:18:13 -0600

    Added zoom functionality, changed user location appearance

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 const MapScreen = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [zoomLevel, setZoomLevel] = useState(12); // Initial zoom level
 
   useEffect(() => {
     (async () => {
@@ -20,16 +21,30 @@ const MapScreen = () => {
     })();
   }, []);
 
+  const handleZoomIn = () => {
+    setZoomLevel(zoomLevel + 1);
+  };
+
+  const handleZoomOut = () => {
+    if (zoomLevel > 0) {
+      setZoomLevel(zoomLevel - 1);
+    }
+  };
+
   return (
     <View style={styles.container}>
       {location ? (
         <MapView
+          provider={MapView.PROVIDER_GOOGLE}
           style={styles.map}
-          initialRegion={{
+          showsUserLocation={true} // Shows the user's location as a blue dot
+          followsUserLocation={true} // Automatically moves the map to the user's location
+          showsMyLocationButton={false} // Hides the default pin marker
+          region={{
             latitude: location.coords.latitude,
             longitude: location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitudeDelta: 0.0922 * Math.pow(2, 12 - zoomLevel),
+            longitudeDelta: 0.0421 * Math.pow(2, 12 - zoomLevel),
           }}
         >
           <Marker
@@ -39,11 +54,19 @@ const MapScreen = () => {
             }}
             title="Your Location"
             description="You are here!"
+            pinColor="blue" // Custom marker color
           />
         </MapView>
       ) : (
         <Text>Loading...</Text>
       )}
+
+      <TouchableOpacity style={styles.zoomInButton} onPress={handleZoomIn}>
+        <Text>+</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.zoomOutButton} onPress={handleZoomOut}>
+        <Text>-</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -51,12 +74,25 @@ const MapScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    flex: 1,
+  },
+  zoomInButton: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+  },
+  zoomOutButton: {
+    position: 'absolute',
+    top: 70,
+    right: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
   },
 });
 
